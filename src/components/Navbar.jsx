57eb4e061023439959db7522a2f26d66ad592fb4
harsh021102 <harsh021102@gmail.com>
2023-01-24 13:49:56 +0530

    decrease size of icon for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,16 +8,16 @@ const Navbar = () => {
     <div className='h-1/12 w-full sm:w-16 md:w-16 rounded-b-xl sm:rounded-none sm:rounded-r-xl sm:h-full flex flex-row md:flex-col justify-center items-center bg-nav-black'>
         <div className='flex sm:flex-col items-center gap-12 sm:gap-8'>
             <button className='my-4 sm:mx-4'>
-                <HiOutlineHome className='text-white text-5xl sm:text-4xl'/>
+                <HiOutlineHome className='text-white text-3xl sm:text-4xl'/>
             </button>
             <button className='sm:mx-4'>
-                <BiUser className='text-white text-5xl sm:text-4xl'/>
+                <BiUser className='text-white text-3xl sm:text-4xl'/>
             </button>
             <button className='sm:mx-4'>
-                <FaGraduationCap className='text-white text-5xl sm:text-4xl'/>
+                <FaGraduationCap className='text-white text-3xl sm:text-4xl'/>
             </button>
             <button className='sm:mx-4'>
-                <BsBriefcase className='text-white text-5xl sm:text-4xl'/>
+                <BsBriefcase className='text-white text-3xl sm:text-4xl'/>
             </button>
         </div>
     </div>
